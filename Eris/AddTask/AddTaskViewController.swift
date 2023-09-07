24172a2373606b6894681a18a3441a0be7b01ace
Dmitrii <navssane@gmail.com>
2023-09-07 22:39:00 +0700

    #2 Added date and time pickers

--- Eris/AddTask/AddTaskViewController.swift
+++ Eris/AddTask/AddTaskViewController.swift
@@ -9,17 +9,50 @@ import UIKit
 
 class AddTaskViewController: UIViewController {
 
+    @IBOutlet weak var timeField: UITextField!
+    @IBOutlet weak var dateField: UITextField!
     @IBOutlet weak var descriptionView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Border for text area
         let borderColor : UIColor = UIColor(red: 0.85, green: 0.85, blue: 0.85, alpha: 1.0)
         descriptionView!.layer.borderColor = borderColor.cgColor
         descriptionView!.layer.borderWidth = 0.3
         descriptionView!.layer.cornerRadius = 5.0
-        // Do any additional setup after loading the view.
+        //Setting up date and time fields
+        
+        let timeFormater = DateFormatter()
+        timeFormater.dateFormat = "hh:mm:ss"
+        let timePicker = UIDatePicker()
+        timePicker.datePickerMode = .time
+        timePicker.addTarget(self, action: #selector(timePickerValueChange), for: UIControl.Event.valueChanged)
+        timePicker.frame.size = CGSize(width: 0, height: 300)
+        timePicker.backgroundColor = UIColor.white
+        timePicker.preferredDatePickerStyle = .wheels
+        timeField.inputView = timePicker
+        
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "dd MMMM YYYY"
+        let datePicker = UIDatePicker()
+        datePicker.minimumDate = Date()
+        datePicker.datePickerMode = .date
+        datePicker.addTarget(self, action: #selector(datePickerValueChange), for: UIControl.Event.valueChanged)
+        datePicker.frame.size = CGSize(width: 0, height: 300)
+        datePicker.backgroundColor = UIColor.white
+        datePicker.preferredDatePickerStyle = .wheels
+        dateField.inputView = datePicker
     }
     
-
+    @objc func timePickerValueChange(sender: UIDatePicker) {
+        let timeFormater = DateFormatter()
+        timeFormater.dateFormat = "hh:mm:ss"
+        timeField.text = timeFormater.string(from: sender.date)
+    }
+    @objc func datePickerValueChange(sender: UIDatePicker) {
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "dd MMMM YYYY"
+        dateField.text = dateFormater.string(from: sender.date)
+    }
     /*
     // MARK: - Navigation
 
