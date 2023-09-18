f3cdfce04c71a4bf2ff17c46484be56337c138da
Dmitrii <navssane@gmail.com>
2023-09-18 17:05:51 +0700

    #5 added basic checkbox

--- Eris/HomeView/TaskViewCell.swift
+++ Eris/HomeView/TaskViewCell.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class TaskViewCell: UITableViewCell {
+    @IBOutlet weak var checkboxButton: UIButton!
     @IBOutlet weak var taskDescriptionLabel: UILabel!
     @IBOutlet weak var taskTimeLabel: UILabel!
     @IBOutlet weak var taskNameLabel: UILabel!
+    var checked = false
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -27,5 +31,21 @@ class TaskViewCell: UITableViewCell {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd.MM HH:mm"
         taskTimeLabel.text = dateFormatter.string(from: withtask.datetime ?? Date())
+        //Setting up checkbox
+        checkboxButton.layer.cornerRadius = 18.0
+        let image = UIImage(systemName: "circle", withConfiguration: UIImage.SymbolConfiguration(pointSize: 28, weight: .medium))
+        checkboxButton.setImage(image, for: .normal)
+        
+        checkboxButton.tintColor = .green
+    }
+    @IBAction func checkbox(_ sender: UIButton) {
+        if checked == false {
+            let image2 = UIImage(systemName: "checkmark.circle", withConfiguration: UIImage.SymbolConfiguration(pointSize: 28, weight: .medium))
+            checkboxButton.setImage(image2, for: .normal)
+        } else {
+            let image2 = UIImage(systemName: "circle", withConfiguration: UIImage.SymbolConfiguration(pointSize: 28, weight: .medium))
+            checkboxButton.setImage(image2, for: .normal)
+        }
+        checked = !checked
     }
 }
