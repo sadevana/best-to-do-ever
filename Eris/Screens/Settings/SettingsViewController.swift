fbfe4b6cf948ce27ec041ad8083935cb3a0592a4
Dmitrii <navssane@gmail.com>
2023-10-18 02:34:18 +0700

    Added separate window for character pick

--- Eris/Screens/Settings/SettingsViewController.swift
+++ Eris/Screens/Settings/SettingsViewController.swift
@@ -15,6 +15,20 @@ class SettingsViewController: UIViewController {
 
     var lunaChooseButton = CompanionPickButton(companion: enumCompanions.Luna.getModel(), frame: CGRect(x: 0, y: 0, width: 70, height: 85))
     var claraChooseButton = CompanionPickButton(companion: enumCompanions.Clara.getModel(), frame: CGRect(x: 80, y: 0, width: 70, height: 85))
+    var watchedCompanion: CompanionModel?
+    var companionPickView: UIView = {
+        let view = UIView()
+        view.frame = CGRect(x: 0, y: 0, width: 300, height: 400)
+        view.layer.cornerRadius = 30
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 0.5
+        view.layer.shadowOffset = CGSize.zero
+        view.layer.masksToBounds = false
+        view.layer.shadowRadius = 2
+        view.layer.zPosition = 1.0
+        view.backgroundColor = chosenCompanion.shared.companion.primaryColor
+        return view
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -34,17 +48,63 @@ class SettingsViewController: UIViewController {
         choiceView.addSubview(claraChooseButton)
         lunaChooseButton.addTarget(self, action: #selector(updateChoice), for: .touchUpInside)
         claraChooseButton.addTarget(self, action: #selector(updateChoice), for: .touchUpInside)
+        
     }
     @objc func updateChoice(sender: CompanionPickButton!) {
-        chosenCompanion.shared.companion = sender.companion
-        UserDefaults.standard.set(sender.companion.name, forKey: "Companion")
-        AlertView.instance.refreshAvatar()
-        self.view.backgroundColor = chosenCompanion.shared.companion.primaryColor
-        refreshButtons()
+        setupShopView(companion: sender.companion)
+        
+        //companionPickView.backgroundColor = .black
+        
+    }
+    func setupShopView(companion: CompanionModel) {
+        self.view.addSubview(companionPickView)
+        companionPickView.backgroundColor = companion.primaryColor
+        companionPickView.frame = CGRect(x: view.frame.size.width/2 - 150, y: view.frame.size.height/2 - 250, width: 300, height: 420)
+        let imageCompanion = companion.defaultImage
+        let imageView = UIImageView(frame: CGRect(x: 75, y: 20, width: 150, height: 300))
+        imageView.image = imageCompanion
+        companionPickView.addSubview(imageView)
+        let crossButton = UIButton(frame: CGRect(x: 260, y: 20, width: 30, height: 30))
+        crossButton.setImage(UIImage(systemName: "xmark"), for: .normal)
+        crossButton.tintColor = .black
+        crossButton.addTarget(self, action: #selector(closeChoise), for: .touchUpInside)
+        companionPickView.addSubview(crossButton)
+        var shopLabel = UILabel(frame: CGRect(x: 20, y: 330, width: 260, height: 18))
+        if companion.name == "Luna" {
+            shopLabel.text = "Your faithful default companion!"
+        }
+        if companion.name == "Clara" {
+            shopLabel.text = "Things are very blue!"
+        }
+        shopLabel.textAlignment = .center
+        companionPickView.addSubview(shopLabel)
+        var chooseButton = UIButton(frame: CGRect(x: 75, y: 365, width: 150, height: 35))
+        chooseButton.backgroundColor = companion.darkToneColor
+        chooseButton.layer.cornerRadius = 10
+        chooseButton.setTitle("Pick companion", for: .normal)
+        watchedCompanion = companion
+        chooseButton.addTarget(self, action: #selector(chooseAndClose), for: .touchUpInside)
+        companionPickView.addSubview(chooseButton)
+        
     }
     func refreshButtons() {
         lunaChooseButton.initialSetup()
         claraChooseButton.initialSetup()
+    }
+    @objc func closeChoise(sender: UIButton!) {
+        companionPickView.subviews.map({ $0.removeFromSuperview() })
+        companionPickView.removeFromSuperview()
+        
+    }
+    @objc func chooseAndClose(sender: UIButton!) {
+        chosenCompanion.shared.companion = watchedCompanion!
+        UserDefaults.standard.set(watchedCompanion!.name, forKey: "Companion")
+        AlertView.instance.refreshAvatar()
+        self.view.backgroundColor = chosenCompanion.shared.companion.primaryColor
+        refreshButtons()
+        companionPickView.subviews.map({ $0.removeFromSuperview() })
+        companionPickView.removeFromSuperview()
+        
     }
     /*@IBAction func lunaChosen(_ sender: Any) {
         chosenCompanion.shared.companion = enumCompanions.Luna.getModel()
