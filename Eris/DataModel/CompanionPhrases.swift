1a45bffb4a995a975cdfa22425d944030cb0dabf
Rubella <lina-invers95@yandex.ru>
2023-10-25 18:16:17 +0300

    Update CompanionPhrases.swift
    Adde a bit more phrases. Checked grammar for existed

--- Eris/DataModel/CompanionPhrases.swift
+++ Eris/DataModel/CompanionPhrases.swift
@@ -1,5 +1,5 @@
 //
-//  ompanionPhrases.swift
+//  СompanionPhrases.swift
 //  Eris
 //
 //  Created by Dmitry Chicherin on 24/10/2566 BE.
@@ -9,32 +9,57 @@ import Foundation
 
 
 let lunaChatter = [
-    "What is your favourite food?" : "I kinda like stakes, he-he",
+    "What is your favorite food?" : "I kinda like steaks, he-he",
     "What do you like doing in your free time?" : "I like swimming! And eating!",
-    "What is your favourite season?" : "I like summer the most! It's when I feel the most energetic",
-    "What do you think about deadlines?" : "Deadlines are awful! They always somehow make me do things last moment",
-    "How do you always stay so energetic?": "I always have positive oulook on things! Being optimistic is a great power",
+    "What is your favorite season?" : "I like summer the most! It's when I feel the most energetic",
+    "What do you think about deadlines?" : "Deadlines are awful! They always somehow make me do things at the last moment",
+    "How do you always stay so energetic?": "I always have a positive outlook on things! Being optimistic is a great power",
     "What kind of quests do you like?": "I like things that involve going somewhere, especially new places!",
     "Do you have a dream?": "I want to win an eating contest         ....And to help you become the best version of yourself!",
     "What do you think I should do today?": "I think you should just live your life to the fullest, just like any other day",
     "Do you have any family?": "I have a big family and 6 siblings! Sometimes it gets too loud at home, even for me",
     "How old are you?": "That's a secret!",
     "Do you like cats or dogs more?": "I'm a dog person! Dogs are much more fun to play with",
-    "When is your birthday?": "It's on July the second!"
+    "When is your birthday?": "It's on July 2nd!",
+    "What's your favorite type of music?": "I love upbeat tunes! They always get me in the adventure mood",
+    "What's the best advice you've ever received?" : "Someone once told me, 'Always be curious and never stop exploring!' I've lived by that ever since",
+    "Do you have a favorite book?" : "I don't read as much as I should, but I love tales of grand adventures and hidden treasures!",
+    "Where would you like to travel?" : "Everywhere! There are so many places to explore and mysteries to uncover",
+    "What's your favorite color?" : "Can you guess? It's green, just like my hair!",
+    "Do you like the night or day more?" : "I love the daytime because it's perfect for adventures. But the night sky is so beautiful too!",
+    "How do you handle fear?" : "By facing it head-on! Every challenge is an opportunity, after all",
+    "What makes you laugh?" : "Silly jokes, unexpected surprises, and seeing someone smile",
+    "Do you have a favorite game?" : "I enjoy treasure hunts and puzzles! They keep my mind sharp",
+    "What inspires you?" : "Seeing people push their limits and achieve their dreams. It's so motivating!",
+    "How do you relax after a long day?" : "Listening to the sounds of nature and daydreaming about the next big adventure",
+    "Do you have a motto you live by?" : "Adventure awaits around every corner!"
 ]
 let claraChatter = [
-    "What is your favourite food?" : "I enjoy desserts, escpecially Mont Blanc",
-    "What do you like doing in your free time?" : "I like reading and taking care of my pets!",
-    "What is your favourite season?" : "I really enjoy spring. I feel really refreshed when nature starts waking up",
-    "Do you have any productivity tips?" : "When I feel down I just do things by putting minimum effort in. It helps me to stay consistent!",
-    "What kind of books do you like?": "I like fantasy and non-fiction books!",
-    "What kind of quests do you like?": "I like things that are slower paced and relaxed, even if others may consider them boring",
-    "Do you have a dream?": "I want to write a book of my own someday!",
-    "Do you have any pets?": "I have two cats and a pet turtle!",
-    "When is your birthday?": "It's on 12 of February!",
-    "Tell me some random fact": "Sea otters are only marine animals capable of lifting and turning over rocks",
-    "Tell me a random fact": "Even though nothing can escape black holes, we think that they still slowly evaporate",
-    "Tell me a random fact ": "English language used to have grammatical gender back in the days",
-    "Tell me a random fact  ": "Name of cocktail \"Blue Hawaii\" is related to the film of the same name",
-    "Do you prefer tea or coffee?": "I like tea with milk! Coffee makes it hard for me to fall asleep"
+    "What is your favorite food?" : "I enjoy desserts, especially Mont Blanc",
+    "What do you like doing in your free time?" : "I like reading and taking care of my pets",
+    "What is your favorite season?" : "I really enjoy spring. I feel so refreshed when nature starts waking up",
+    "Do you have any productivity tips?" : "When I feel down, I just do things by putting in minimal effort. It helps me stay consistent!",
+    "What kind of books do you like?" : "I prefer fantasy and non-fiction books",
+    "What kind of quests do you like?" : "I like things that are slower-paced and relaxed, even if others might find them boring",
+    "Do you have a dream?" : "I hope to write my own book someday",
+    "Do you have any pets?" : "I have two cats and a pet turtle",
+    "When is your birthday?" : "It's on February 12th",
+    "Tell me a random fact" : "Sea otters are the only marine animals capable of lifting and turning over rocks",
+    "Tell me a random fact " : "Though nothing can escape black holes, we believe they still slowly evaporate",
+    "Tell me a random fact  " : "The English language used to have grammatical gender long ago",
+    "Tell me a random fact   " : "The cocktail name 'Blue Hawaii' is tied to the film of the same name",
+    "Do you prefer tea or coffee?" : "I prefer tea with milk! Coffee makes it hard for me to fall asleep",
+    "What's your favorite time of day?" : "I love the early morning, just before sunrise. The world feels so calm and peaceful",
+    "What's a hobby you'd like to try?" : "I've always been curious about pottery. Shaping clay seems so therapeutic",
+    "How do you relax after a long day?" : "I enjoy listening to soft music while sipping on chamomile tea",
+    "What's your favorite type of weather?" : "A light drizzle with an overcast sky. Perfect for reading indoors",
+    "What inspires you?" : "Nature, literature, and the kindness of strangers I encounter",
+    "Which place would you love to visit?" : "I've always dreamt of seeing the cherry blossoms",
+    "Do you like indoor or outdoor activities more?" : "I lean more towards indoor activities, like reading or painting",
+    "What's your favorite flower?" : "I love lilies, especially the white ones. They're so elegant and fragrant",
+    "How do you handle stress?" : "Taking deep breaths, meditating, and sometimes just taking a step back to re-evaluate",
+    "What's a book you've read recently?" : "I just finished 'The Night Circus'. It's enchanting and beautifully written",
+    "Do you enjoy puzzles or riddles?" : "Yes, especially the ones that make me think for days",
+    "What's your favorite scent?" : "I love the scent of fresh linen and lavender. They're so calming",
+    "How do you feel about technology?" : "It has its pros and cons. I appreciate the convenience, but sometimes, I miss the simplicity of the past"
 ]
