53e9036a3a29f3b3e43aec9261824dda912faaf9
Dmitrii <navssane@gmail.com>
2023-10-30 01:12:18 +0700

    Added interface elements and options for repetitions

---
+++ Eris/DataModel/RepeatSettings.swift
@@ -0,0 +1,19 @@
+//
+//  RepeatSettings.swift
+//  Eris
+//
+//  Created by Dmitry Chicherin on 30/10/2566 BE.
+//
+
+import Foundation
+
+enum Repeats: String, CaseIterable {
+case everyday = "Every day"
+    case everyMonday = "Every Monday"
+    case everyTuesday = "Every Tuesday"
+    case everyWednesday = "Every Wednesday"
+    case everyThursday = "Every Thursday"
+    case everyFriday = "Every Friday"
+    case everySaturday = "Every Saturday"
+    case everySunday = "Every Sunday"
+}
