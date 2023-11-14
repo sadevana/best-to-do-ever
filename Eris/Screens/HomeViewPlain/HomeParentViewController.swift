af51d7f4ad224c4ed71ba2c9f675d2ff56da4c7e
Dmitrii <navssane@gmail.com>
2023-11-14 18:15:35 +0700

    Improved search bar

--- Eris/Screens/HomeViewPlain/HomeParentViewController.swift
+++ Eris/Screens/HomeViewPlain/HomeParentViewController.swift
@@ -93,23 +93,25 @@ class HomeParentViewController: UIViewController {
             if let childVC = self.children.first as? HomeTableViewController {
                 //childVC.activateSearch()
                 searchBar.searchBarStyle = UISearchBar.Style.default
-                searchBar.placeholder = " Search..."
+                searchBar.placeholder = " Title or description"
+                
                 searchBar.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: 45)
                 searchBar.translatesAutoresizingMaskIntoConstraints = false
-                //searchBar.widthAnchor.constraint(equalToConstant: 150).isActive = true
-                //searchBar.heightAnchor.constraint(equalToConstant: 150).isActive = true
-                //searchBar.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
                 
-                self.children.first?.view.frame = (self.children.first?.view.frame.insetBy(dx: 0, dy: 45))!
-                searchBar.isTranslucent = false
-                searchBar.backgroundImage = UIImage()
-                searchBar.barTintColor = .white//chosenCompanion.shared.companion.primaryColor
-                searchBar.searchTextField.tintColor = .white
+                UIView.animate(withDuration: 0.3) {
+                    self.children.first?.view.frame = (self.children.first?.view.frame.insetBy(dx: 0, dy: 45))!
+                }
+                
+                //searchBar.searchTextField.tintColor = .red
                 searchBar.delegate = self
                 searchBar.searchTextField.delegate = self
                 searchBar.searchTextField.shouldResignOnTouchOutsideMode = .enabled
-                
-                self.view.addSubview(searchBar)
+                searchBar.searchTextField.backgroundColor = .white
+                self.view.addSubview(self.searchBar)
+                searchBar.alpha = 0
+                UIView.animate(withDuration: 0.3) {
+                    self.searchBar.alpha = 1.0
+                }
                 //Attaching a searchbar to the top of a table view
                 searchBar.bottomAnchor.constraint(equalTo: (self.children.first?.view.topAnchor)!, constant: 0).isActive = true
                 searchBar.widthAnchor.constraint(equalToConstant: view.frame.size.width).isActive = true
@@ -118,6 +120,12 @@ class HomeParentViewController: UIViewController {
                 searchBar.searchTextField.clearButtonMode = .never
                 searchBar.searchTextField.rightViewMode = .always
                 searchBar.searchTextField.rightView = clearButton
+                
+                searchBar.barTintColor = UIColor.white
+                searchBar.backgroundColor = UIColor.clear
+                searchBar.isTranslucent = true
+                searchBar.setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
+                
                 searchBar.becomeFirstResponder()
             }
         }
@@ -126,12 +134,25 @@ class HomeParentViewController: UIViewController {
     @objc func clearSearch(sender: UIButton!) {
         searchBar.searchTextField.text = ""
         searchBar.resignFirstResponder()
-        searchBar.removeFromSuperview()
+
+        UIView.animate(withDuration: 0.2, animations: {
+            
+            self.searchBar.alpha = 0.0
+            
+             }, completion: {
+                 finished in
+                 self.searchBar.removeFromSuperview()
+             })
         searchMode = false
         if let childVC = self.children.first as? HomeTableViewController {
             childVC.filterData(searchText: "")
         }
-        self.children.first?.view.frame = (self.children.first?.view.frame.insetBy(dx: 0, dy: -45))!
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            UIView.animate(withDuration: 0.3) {
+                self.children.first?.view.frame = (self.children.first?.view.frame.insetBy(dx: 0, dy: -45))!
+            }
+        }
     }
     @objc func goToSettings(tapGestureRecognizer: UIGestureRecognizer!) {
         let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
